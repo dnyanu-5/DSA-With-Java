d005df198b62d23df3661049477b3c3608409622
Dnyanu Sonawane <dnyasonawane5804@gmail.com>
2025-07-16 15:19:06 +0530

    armstrong number

--- armstrong_number.java
+++ armstrong_number.java
@@ -4,6 +4,7 @@ public class armstrong_number {
     public static void main(String[] args) {
 
         int num = 153;
+        int original = num;
         int armNum = 0;
         while (num > 0) {
             int lastDigit = num % 10;
@@ -11,10 +12,10 @@ public class armstrong_number {
             num /= 10;
         }
 
-        if(num == armNum){
+        if(armNum == original){
             System.out.println("The number is Armstrong number");
         }else{
-            System.out.println("The number is Armstrong number");
+            System.out.println("The number is not a Armstrong number");
         }
     }
 
