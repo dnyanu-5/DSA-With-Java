cf60470e15a5e15785ed948413de957767793b54
Dnyanu Sonawane <dnyasonawane5804@gmail.com>
2025-07-19 11:55:32 +0530

    added smallest number in an array code

--- Arrays/LargeNumber.java
+++ Arrays/LargeNumber.java
@@ -1,20 +1,32 @@
 // Largest number in an array 
-import java.util.*;
+
 public class LargeNumber {
 
     public static int LargestNumber(int arr[]) {
         int largeNum = Integer.MIN_VALUE;
         for (int i = 0; i < arr.length; i++) {
-            if(arr[i] > largeNum){
+            if (arr[i] > largeNum) {
                 largeNum = arr[i];
             }
         }
         return largeNum;
     }
 
+    public static int SmallestNumber(int arr[]) {
+        int SmallNum = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < SmallNum) {
+                SmallNum = arr[i];
+            }
+        }
+        return SmallNum;
+    }
+
     public static void main(String[] args) {
-        int array[] = { 23, 45, 78, 98, 34,10, 55 };
-        System.out.println("The largest number in an array is:"+ LargestNumber(array));
+        int array[] = { 23, 45, 78, 98, 34, 10, 55 };
+        System.out.println("The largest number in an array is:" + LargestNumber(array));
+
+        System.out.println("The smallest number in an array is:" + SmallestNumber(array));
 
     }
 
