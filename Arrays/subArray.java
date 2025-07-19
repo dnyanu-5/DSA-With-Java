4060531636a365358f9efbe94a8bccd61d827084
Dnyanu Sonawane <dnyasonawane5804@gmail.com>
2025-07-19 13:43:58 +0530

    adding pairs in an array and subArray code

---
+++ Arrays/subArray.java
@@ -0,0 +1,47 @@
+// how many pairs in the array
+public class subArray{
+    public static void pairsInArray(int arr[]) {
+        int totalPair = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                System.out.print("(" + arr[i] + "," + arr[j] + ")");
+                totalPair++;
+            }
+            System.out.println();
+        }
+        System.out.println("Total pairs in an array is:" + totalPair);
+    }
+    // pairs in array FORMULA = n(n-1)/2
+
+     //subarray in array FORMULA = n(n+1)/2
+     public static void subArrays(int arr[]) {
+        int totalArr = 0;
+        int minSum = Integer.MAX_VALUE;
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                int totalSum = 0;
+                for (int k = i; k <= j; k++) {
+                    System.out.print(arr[k] + " ");
+                    totalSum += arr[k];
+                }
+                totalArr++;
+                System.out.println("=> Sum: " + totalSum);
+
+                if (totalSum < minSum) minSum = totalSum;
+                if (totalSum > maxSum) maxSum = totalSum;
+            }
+            System.out.println(); // spacing between starting index i
+        }
+
+        System.out.println("Minimum subarray sum: " + minSum);
+        System.out.println("Maximum subarray sum: " + maxSum);
+        System.out.println("Total subarrays: " + totalArr);
+    }
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 4, 5 };
+        pairsInArray(arr);
+        subArrays(arr);
+    }
+}
