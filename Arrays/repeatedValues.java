684958687dcbc362b3864231048d80af13b381fa
Dnyanu Sonawane <dnyasonawane5804@gmail.com>
2025-07-21 17:10:58 +0530

    check if value appears twice or distinct

---
+++ Arrays/repeatedValues.java
@@ -0,0 +1,16 @@
+public class repeatedValues {
+    public static boolean values(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]==arr[j]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5};
+        System.out.println(values(arr));
+    }
+}
