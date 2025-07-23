82aa96c5bf64622f8038b356aa8354f74631a6b0
Dnyanu Sonawane <dnyasonawane5804@gmail.com>
2025-07-23 12:35:52 +0530

    sort function

--- Sorting_Algoritham/buildInSort.java
+++ Sorting_Algoritham/buildInSort.java
@@ -9,8 +9,8 @@ public class buildInSort {
     }
     public static void main(String[] args) {
         int arr[]={2,5,1,3,4,6};
-        // Arrays.sort(arr);
-        // printArr(arr);
+        Arrays.sort(arr);
+        printArr(arr);
         Arrays.sort(arr,0,3);
         printArr(arr);
     }   
