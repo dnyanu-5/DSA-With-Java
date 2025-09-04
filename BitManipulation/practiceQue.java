2742f73a0727a933d8a7bd4d545dbb1b337bfd1b
Dnyanu Sonawane <dnyasonawane5804@gmail.com>
2025-09-04 22:29:41 +0530

    practice q for bit manipulation

---
+++ BitManipulation/practiceQue.java
@@ -0,0 +1,31 @@
+package BitManipulation;
+public class practiceQue {
+    public static void main(String[] args) {
+        //1. x^x for any value of x
+        int a=6;
+        System.out.println(a ^ a);
+
+        //2 swap two numbers
+        int x=5;int y=4;
+        System.out.println("x="+x +","+"y="+y);
+        x=x^y;
+        y=x^y;
+        x=x^y;
+        System.out.println("x="+x +","+"y="+y);
+
+        // add 1 to one integer
+       System.out.println("adding one to an integer: "+ -~a);
+
+       //convert uppercase to lowercase
+       
+       for(char ch='A';ch<='Z';ch++){
+        System.out.print((char)(ch | ' ')+" ");
+       }
+
+
+
+
+
+
+    }
+}
