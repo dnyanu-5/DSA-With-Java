bfbb3e596af8a247a4e566bab29e22e7b45c0a18
Dnyanu Sonawane <dnyasonawane5804@gmail.com>
2025-10-14 22:07:44 +0530

    basics of backtracking

---
+++ BackTracking/changeArr.java
@@ -0,0 +1,30 @@
+package BackTracking;
+
+public class changeArr {
+
+    public static void changeArray(int arr[],int i,int val){
+        //base case
+        if(i==arr.length){
+            printArr(arr);
+            return;
+        }
+        //recursion
+
+        arr[i]=val;
+        changeArray(arr, i+1, val+1);
+        arr[i]=val-1;
+    }
+
+
+    public static void printArr(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[]=new int[5];
+        changeArray(arr, 0, 1);
+        printArr(arr);
+    }
+}
---
+++ BackTracking/subsets.java
@@ -0,0 +1,22 @@
+package BackTracking;
+
+public class subsets {
+    public static void findsubsets(String str, String ans, int i) {
+        if (i == str.length()) {
+            if (ans.length() == 0) {
+                System.out.println("null");
+            } else {
+                System.out.println(ans);
+            }
+            return;
+        }
+        findsubsets(str, ans + str.charAt(i), i + 1);
+        findsubsets(str, ans, i + 1);
+    }
+
+    public static void main(String[] args) {
+        String str = "xyz";
+        findsubsets(str, "", 0);
+    }
+
+}
