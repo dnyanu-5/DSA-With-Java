54f34757320bbb27f0435f531cfdfd21954599e7
Dnyanu Sonawane <dnyasonawane5804@gmail.com>
2025-10-18 11:27:33 +0530

    backtracaking in permutation

---
+++ BackTracking/permutation.java
@@ -0,0 +1,22 @@
+package BackTracking;
+
+public class permutation {
+    public static void findPermutation(String str, String ans) {
+        // base case
+        if (str.length() == 0) {
+            System.out.println(ans);
+            return;
+        }
+        // choices of letters
+        for (int i = 0; i < str.length(); i++) {
+            char currChar = str.charAt(i);
+            String NewStr = str.substring(0, i) + str.substring(i + 1); // remove ith index
+            findPermutation(NewStr, ans + currChar);
+        }
+    }
+
+    public static void main(String[] args) {
+        String str = "abc";
+        findPermutation(str, "");
+    }
+}
