1c084280219987ef2282829a4ae68f3db73c8aba
Dnyanu Sonawane <dnyasonawane5804@gmail.com>
2026-03-06 22:48:06 +0530

    Dijkstra algorithm

---
+++ Graphs/AllPaths.java
@@ -0,0 +1,53 @@
+package Graphs;
+import java.util.*;
+
+public class AllPaths {
+
+    static class Edge {
+        int src;
+        int dest;
+
+        public Edge(int s, int d) {
+            this.src = s;
+            this.dest = d;
+        }
+    }
+
+    static void createGraph(ArrayList<Edge> graph[]) {
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        graph[2].add(new Edge(2, 3));
+        graph[0].add(new Edge(0, 3));
+
+        graph[3].add(new Edge(3, 1));
+
+        graph[4].add(new Edge(4, 0));
+        graph[4].add(new Edge(4, 1));
+
+        graph[5].add(new Edge(5, 0));
+        graph[5].add(new Edge(5, 2));
+    }
+    public static void AllPathFromStoD(ArrayList<Edge> graph[],int src,int des, String path){
+        if(src==des){
+            System.err.println(path+des);
+            return;
+        }
+
+        for(int i=0;i<graph[src].size();i++){
+            Edge e= graph[src].get(i);
+            AllPathFromStoD(graph, e.dest, des, path+src);
+        }
+    }
+    public static void main(String[] args) {
+        int V = 6;
+        @SuppressWarnings("unchecked")
+        ArrayList<Edge> graph[] = new ArrayList[V];
+        createGraph(graph);
+
+        int src=4;
+        int des=2;
+        AllPathFromStoD(graph, src,des, "");
+    }
+
+}
---
+++ Graphs/Dijkstras.java
@@ -0,0 +1,98 @@
+package Graphs;
+
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
+public class Dijkstras {
+    static class Edge {
+        int src;
+        int dest;
+        int wt;
+
+        public Edge(int s, int d, int wt) {
+            this.src = s;
+            this.dest = d;
+            this.wt = wt;
+        }
+    }
+
+    static class Pair implements Comparable<Pair> {
+        int n;
+        int dist;
+
+        Pair(int node, int dist) {
+            this.n = node;
+            this.dist = dist;
+        }
+
+        @Override
+        public int compareTo(Pair p2) {
+            return this.dist - p2.dist;
+        }
+    }
+
+    public static void dijikstraAlgo(ArrayList<Edge> graph[], int src) {
+        // 1. store infinity value
+        int dist[] = new int[graph.length];
+        for (int i = 0; i < graph.length; i++) {
+            if (i != src) {
+                dist[i] = Integer.MAX_VALUE;
+            }
+        }
+        // 2 pq
+        boolean vis[] = new boolean[graph.length];
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+
+        pq.add(new Pair(src, 0));
+
+        // 3 bfs
+        while (!pq.isEmpty()) {
+            Pair curr = pq.remove();
+            if (!vis[curr.n]) {
+                vis[curr.n] = true;
+
+                for (int i = 0; i < graph[curr.n].size(); i++) {
+                    Edge e = graph[curr.n].get(i);
+                    int u = e.src;
+                    int v = e.dest;
+                    int wt = e.wt;
+
+                    if (dist[u] + wt < dist[v]) {
+                        dist[v] = dist[u] + wt;
+                        pq.add(new Pair(v, dist[v]));
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < dist.length; i++) {
+            System.out.print(dist[i] + " ");
+        }
+    }
+
+    static void createGraph(ArrayList<Edge> graph[]) {
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        graph[0].add(new Edge(0, 1, 2));
+        graph[0].add(new Edge(0, 2, 4));
+
+        graph[1].add(new Edge(1, 3, 7));
+        graph[1].add(new Edge(1, 2, 1));
+
+        graph[2].add(new Edge(2, 4, 3));
+
+        graph[3].add(new Edge(3, 5, 1));
+
+        graph[4].add(new Edge(4, 5, 5));
+        graph[4].add(new Edge(4, 3, 2));
+    }
+
+    public static void main(String[] args) {
+        int V = 6;
+        @SuppressWarnings("unchecked")
+        ArrayList<Edge>[] graph = new ArrayList[V];
+
+        createGraph(graph);
+        dijikstraAlgo(graph, 0);
+    }
+}
